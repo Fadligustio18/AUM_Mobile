aa60cfcc72c953e1b1ac8b8987f30b2436df1bbf
Fadligustio18 <fadligustio@gmail.com>
2026-08-19 13:19:09 +0700

    desain layout 0.1

--- app/src/main/java/com/example/bknova/service/Aktor.kt
+++ app/src/main/java/com/example/bknova/service/Aktor.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Aktor {
-    private const val BASE_URL = "http://192.168.69.41:3000/"
+    private const val BASE_URL = "http://192.168.69.50:3000/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
