3300f6e27a3f431409fd5760049584b2f931323d
Fadligustio18 <fadligustio@gmail.com>
2026-09-04 23:46:38 +0700

    update

--- app/src/main/java/com/example/bknova/service/Aktor.kt
+++ app/src/main/java/com/example/bknova/service/Aktor.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Aktor {
-    private const val BASE_URL = "http://192.168.69.50:3000/"
+    private const val BASE_URL = "https://throw-headquarters-losses-anytime.trycloudflare.com"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
