195ae59cd25928f147218573d67a1c6e1c810cae
Fadligustio18 <fadligustio@gmail.com>
2026-09-17 15:23:38 +0700

    UPDATE

--- app/src/main/java/com/example/bknova/service/Aktor.kt
+++ app/src/main/java/com/example/bknova/service/Aktor.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Aktor {
-    private const val BASE_URL = "http://100.77.23.34:3000"
+    private const val BASE_URL = "https://bk-nova.tefa-bcs.org"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
